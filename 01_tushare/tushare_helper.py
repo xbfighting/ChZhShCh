cff651fefcb17a6ed8afdd3bc247f13241fad851
baileicheng <baileicheng@exfresh.com.cn>
2017-12-07 15:53:03 +0800

    [mod] bar 方法私有化，删除测试代码，tickers 新增

--- 01_tushare/tushare_helper.py
+++ 01_tushare/tushare_helper.py
@@ -32,11 +32,13 @@ class TushareHelper(object):
         self.data_frame = {}
         self.data_original = []
         self.data_original_ex = []
+        self.date_tickers = []
 
-    def bar(self):
+    def __bar(self):
         self.data_frame = ts.bar(code=self.code, conn=TushareConn.conn, start_date=self.start, end_date=self.end,
                                  freq=self.freq, asset=self.asset)
-    def standardized(self):
+    def data_transfer(self):
+        self.__bar()
         list_index = 0
         # data_frame 按照 index 倒叙排序
         for index, row in self.data_frame.sort_index().iterrows():
@@ -45,14 +47,5 @@ class TushareHelper(object):
             row["is_up"] = row["open"] <= row["close"]
             self.data_original_ex.append((list_index, row['open'], row['high'], row['low'], row['close']))
             self.data_original.append(row)
+            self.date_tickers.append(str(row['index']))
             list_index += 1
-
-# test
-test = TushareHelper('000001', '2017-12-04','2017-12-05','30min')
-test.bar()
-test.standardized()
-
-for item in test.data_original:
-    print(item)
-for item in test.data_original_ex:
-    print(item)
