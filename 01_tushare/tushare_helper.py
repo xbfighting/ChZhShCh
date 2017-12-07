33870aecbc9f83b1531ad6ac2d768466dc7a429b
baileicheng <baileicheng@exfresh.com.cn>
2017-12-07 15:52:23 +0800

    [mod] 单例简化

--- 01_tushare/tushare_helper.py
+++ 01_tushare/tushare_helper.py
@@ -10,7 +10,7 @@ class Singleton(object):
 
     def __new__(cls, *args, **kw):
         if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kw)
+            cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
 
