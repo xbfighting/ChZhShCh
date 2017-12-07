f995e5bb67215bae6679a99052be9f0ed8602f28
baileicheng <baileicheng@exfresh.com.cn>
2017-12-07 15:53:18 +0800

    [mod] import test

--- 01_tushare/tushare_connection.py
+++ 01_tushare/tushare_connection.py
@@ -1,8 +1,13 @@
+import sys
 import json
-
 from datetime import datetime
+#F:\GitHub\ChZhShCh_Python\00_common\__frequency_enum__\frequency_enum.py
+#sys.path.append(r"F:\GitHub\ChZhShCh_Python\01_tushare")
+#sys.path.append(r"")
+import tushare_helper
 
 
+# frequency_enum.
 class Object:
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
