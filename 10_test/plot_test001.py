ffd720bf8683393369e3d39fe2e4847eb7983b59
baileicheng <baileicheng@exfresh.com.cn>
2017-12-18 14:35:50 +0800

    [mod] plot 平滑曲线test

--- 10_test/plot_test001.py
+++ 10_test/plot_test001.py
@@ -5,12 +5,25 @@ import matplotlib.finance as mpf
 from pandas import Series, DataFrame
 from matplotlib.pylab import date2num
 # %matplotlib inline
+#
+# ser = Series(np.random.randn(24))
+#
+# print(ser)
+#
+# ser[:8].plot(color='red')
+# ser[7:16].plot(color='green')
+# ser[15:24].plot(color='blue')
+# plt.show()
 
-ser = Series(np.random.randn(24))
 
-print(ser)
+x = np.linspace(-3, 3, 50)
+y1 = 2*x + 1
+y2 = x**2
 
-ser[:8].plot(color='red')
-ser[7:16].plot(color='green')
-ser[15:24].plot(color='blue')
+print(y1)
+print(y2)
+plt.figure()
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(y1, y2)
 plt.show()
