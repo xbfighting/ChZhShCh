5669c7b73cea2d1a127d8a96c7e6a3a7a11bec48
baileicheng <baileicheng@exfresh.com.cn>
2017-12-18 15:12:52 +0800

    [mod] main test adjust

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import standardized as standard
 sys.path.append(r"09_show")
 import show
 
-original = th.TushareHelper('000001', datetime.date.today()+ datetime.timedelta(days=-1),datetime.date.today() + datetime.timedelta(days=+1),'1min')
-original = th.TushareHelper('000001', '2017-12-04', '2017-12-12','1min')
+# original = th.TushareHelper('000001', datetime.date.today()+ datetime.timedelta(days=-1),datetime.date.today() + datetime.timedelta(days=+1),'1min')
+original = th.TushareHelper('000001', '2017-12-04', '2017-12-12','D')
 original.data_transfer()
 
 sta = standard.StandardHandle(original.data_original)
