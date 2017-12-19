b82ef467556746d45e632e39632470b993038007
baileicheng <xbfighting@hotmail.com>
2017-12-19 13:50:30 +0800

    [mod] delete series test

--- 10_test/plot_test001.py
+++ 10_test/plot_test001.py
@@ -4,17 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.finance as mpf
 from pandas import Series, DataFrame
 from matplotlib.pylab import date2num
-# %matplotlib inline
-#
-# ser = Series(np.random.randn(24))
-#
-# print(ser)
-#
-# ser[:8].plot(color='red')
-# ser[7:16].plot(color='green')
-# ser[15:24].plot(color='blue')
-# plt.show()
-
 
 x = np.linspace(-3, 3, 50)
 y1 = 2*x + 1
