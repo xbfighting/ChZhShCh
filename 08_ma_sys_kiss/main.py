f41094c270243111301975745abb001a1e03abbb
baileicheng <xbfighting@hotmail.com>
2017-12-20 16:59:52 +0800

    [mod] 均线+吻的main

---
+++ 08_ma_sys_kiss/main.py
@@ -0,0 +1,18 @@
+import sys
+import datetime
+sys.path.append(r"../01_tushare")
+import tushare_helper as th
+sys.path.append(r"../02_candel_standardized")
+import standardized as standard
+sys.path.append(r"../09_show")
+import show
+sys.path.append(r"../08_ma_sys_kiss")
+import ma_kiss as mk
+
+code = "002500"
+original = th.TushareHelper(code, datetime.date.today()+ datetime.timedelta(days=-30),datetime.date.today(),'60min')
+original.data_transfer_ma()
+
+makiss = mk.MAKiss(code, original.data_frame_ma_mini)
+my_plot = show.PlotShow([], code)
+my_plot.ma_kiss_show(makiss)
