6af7e51e994c1a0c0981d2cc2c00fbda7e8a9fe2
baileicheng <xbfighting@hotmail.com>
2017-12-21 00:01:16 +0800

    [mod] 默认查看今天的数据

--- 08_ma_sys_kiss/main.py
+++ 08_ma_sys_kiss/main.py
@@ -9,8 +9,8 @@ import show
 sys.path.append(r"../08_ma_sys_kiss")
 import ma_kiss as mk
 
-code = "002500"
-original = th.TushareHelper(code, datetime.date.today()+ datetime.timedelta(days=-30),datetime.date.today(),'60min')
+code = "601933"
+original = th.TushareHelper(code, datetime.date.today() + datetime.timedelta(days=-30), datetime.date.today() + datetime.timedelta(days=1),'60min')
 original.data_transfer_ma()
 
 makiss = mk.MAKiss(code, original.data_frame_ma_mini)
