abf313bf2d490436a08448db6cef7e348ea928c1
baileicheng <xbfighting@hotmail.com>
2017-12-20 18:41:25 +0800

    [mod] 交点计算

--- 08_ma_sys_kiss/ma_kiss.py
+++ 08_ma_sys_kiss/ma_kiss.py
@@ -14,7 +14,6 @@ sys.path.append(r"../09_show")
 import show
 
 class MAKiss(object):
-
     # x 轴转换 xtickers 表示
     def __date_tickers_transfer(self):
         self.date_tickers = []
@@ -34,11 +33,23 @@ class MAKiss(object):
         self.x_index = self.data_frame_ma_mini['index']
         self.long = self.data_frame_ma_mini['long']
         self.short = self.data_frame_ma_mini['short']
+        self.long_interp1d = interpolate.interp1d(self.x_index, self.long)
+        self.short_interp1d = interpolate.interp1d(self.x_index, self.short)
+        self.intersection = {'Postural': '', 'X': 0, 'Y': 0}
         self.intersections = []
         self.intersections_ex = []
         self.int_tickers =  np.linspace(0, len(self.x_index), 5)
         self.date_tickers = self.__date_tickers_transfer()
 
 
+    def __get_intersection(pre_short, pre_long, curr_short, curr_long):
+        if np.isnan(pre_short) != True and np.isnan(pre_long) != True and np.isnan(curr_short) != True and np.isnan(curr_long) != True:
+            # if pre_long - pre_short >= 0
+            pass
+        pass
+    def get_intersections(self):
+        pass
 
+# f1 = interpolate.interp1d(x, y1)
+# f2 = interpolate.interp1d(x, y2)
 
