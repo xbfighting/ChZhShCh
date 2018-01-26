aba65214c1e7c9e7725ad6af3395b74eb558d537
baileicheng <xbfighting@hotmail.com>
2018-01-26 17:42:44 +0800

    [mod] 年线选股 init

---
+++ 06_StockPick/year_ma.py
@@ -0,0 +1,11 @@
+import sys
+import datetime
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from scipy import interpolate
+
+pass
+
+# TODO: 获取成交量
