1d6e74dc10d620348dc07681f7aba79fa4aee0c6
baileicheng <xbfighting@hotmail.com>
2018-02-05 17:58:31 +0800

    [mod] 禅中说禅中枢定义

---
+++ 00_common/chzhshch_central.py
@@ -0,0 +1,11 @@
+import sys
+
+# 中枢类
+class Central:
+    def __init__(self, GG, DD, ZG, ZD, start, end):
+        self.GG = GG
+        self.DD = DD
+        self.ZG = ZG
+        self.ZD = ZD
+        self.start = start
+        self.end = end
