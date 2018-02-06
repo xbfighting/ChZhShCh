a776ae8610a3f14c9f9eda60bd464b3e2833aa9b
baileicheng <xbfighting@hotmail.com>
2018-02-06 16:42:12 +0800

    [mod] 格式化代码

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ sys.path.append(r"09_show")
 import show
 
 # original = th.TushareHelper('000001', datetime.date.today()+ datetime.timedelta(days=-1),datetime.date.today() + datetime.timedelta(days=+1),'1min')
-original = th.TushareHelper('000001', '2017-12-04', '2017-12-12','D')
+original = th.TushareHelper('000001', '2017-12-04', '2017-12-12', 'D')
 original.data_transfer()
 
 sta = standard.StandardHandle(original.data_original)
@@ -24,5 +24,5 @@ date_tickers = sta.date_tickers
 my_plot = show.PlotShow(date_tickers, '000001')
 # my_plot.candle_show(sta.standardized_list_ex, [])
 my_plot.candle_show(sta.standardized_list_ex, sta.top_bottom_list_ex)
-my_plot.candle_show(sta.standardized_list_ex, sta.standardized_top_bottom_list_ex)
-
+my_plot.candle_show(sta.standardized_list_ex,
+                    sta.standardized_top_bottom_list_ex)
